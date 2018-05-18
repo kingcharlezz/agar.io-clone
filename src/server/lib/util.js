721dc643225668c4e605f3910e7f140439994b96
kingcharlezz <39387371+kingcharlezz@users.noreply.github.com>
2018-05-18 17:59:49 -0400

    Update util.js

--- src/server/lib/util.js
+++ src/server/lib/util.js
@@ -1,111 +1,98 @@
-/*jshint expr:true */
+/* jslint node: true */
 
-var expect = require('chai').expect,
-    util   = require('../src/server/lib/util');
+'use strict';
 
+var cfg = require('../../../config.json');
 
-/**
- * Tests for server/lib/util.js
- *
- * This is mostly a regression suite, to make sure behavior
- * is preserved throughout changes to the server infrastructure.
- */
+exports.validNick = function(nickname) {
+    var regex = /^\w*$/;
+    return regex.exec(nickname) !== null;
+};
 
-describe('util.js', function () {
-
-  describe('#massToRadius', function () {
-
-    it('should return non-zero radius on zero input', function () {
-      var r = util.massToRadius(0);
-      expect(r).to.be.a('number');
-      expect(r).to.equal(4);
-    });
-
-    it('should convert masses to a circle radius', function () {
-      var r1 = util.massToRadius(4),
-          r2 = util.massToRadius(16),
-          r3 = util.massToRadius(1);
-
-      expect(r1).to.equal(16);
-      expect(r2).to.equal(28);
-      expect(r3).to.equal(10);
-    });
-  });
+// determine mass from radius of circle
+exports.massToRadius = function (mass) {
+    return 4 + Math.sqrt(mass) * 6;
+};
 
 
-  describe('#validNick', function () {
+// overwrite Math.log function
+exports.log = (function () {
+    var log = Math.log;
+    return function (n, base) {
+        return log(n) / (base ? log(base) : 1);
+    };
+})();
 
-    it('should allow empty player nicknames', function () {
-      var bool = util.validNick('');
-      //expect(bool).to.be.true;
-    });
+// get the Euclidean distance between the edges of two shapes
+exports.getDistance = function (p1, p2) {
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)) - p1.radius - p2.radius;
+};
 
-    it('should allow ascii character nicknames', function () {
-      var n1 = util.validNick('Walter_White'),
-          n2 = util.validNick('Jesse_Pinkman'),
-          n3 = util.validNick('hank'),
-          n4 = util.validNick('marie_schrader12'),
-          n5 = util.validNick('p');
+exports.randomInRange = function (from, to) {
+    return Math.floor(Math.random() * (to - from)) + from;
+};
 
-      expect(n1).to.be.true;
-      expect(n2).to.be.true;
-      expect(n3).to.be.true;
-      expect(n4).to.be.true;
-      expect(n5).to.be.true;
-    });
+// generate a random position within the field of play
+exports.randomPosition = function (radius) {
+    return {
+        x: exports.randomInRange(radius, cfg.gameWidth - radius),
+        y: exports.randomInRange(radius, cfg.gameHeight - radius)
+    };
+};
 
-    it('should disallow unicode-dependent alphabets', function () {
-      var n1 = util.validNick('Йèæü');
+exports.uniformPosition = function(points, radius) {
+    var bestCandidate, maxDistance = 0;
+    var numberOfCandidates = 10;
 
-      expect(n1).to.be.false;
-    });
-
-    it('should disallow spaces in nicknames', function () {
-        var n1 = util.validNick('Walter White');
-        expect(n1).to.be.false;
-    });
-  });
-
-  describe('#log', function () {
-
-    it('should compute the log_{base} of a number', function () {
-      var base10 = util.log(1, 10),
-          base2  = util.log(1, 2);
-
-      var identity = util.log(10, 10);
-
-      var logNineThree = Math.round(util.log(9,3) * 1e5) / 1e5; // Tolerate rounding errors
-
-      //  log(1) should equal 0, no matter the base
-      expect(base10).to.eql(base2);
-
-      // log(n,n) === 1
-      expect(identity).to.eql(1);
-
-      // perform a trivial log calculation: 3^2 === 9
-      expect(logNineThree).to.eql(2);
-    });
-
-  });
-
-  describe('#getDistance', function () {
-
-    // helper class
-    function Point(x,y,r) {
-      return {
-        x      : x,
-        y      : y,
-        radius : r
-      };
+    if (points.length === 0) {
+        return exports.randomPosition(radius);
     }
 
-    var p1 = Point(-100, 20, 1),
-        p2 = Point(0, 40, 5),
-        p3 = Point(0,0,100);
+    // Generate the candidates
+    for (var ci = 0; ci < numberOfCandidates; ci++) {
+        var minDistance = Infinity;
+        var candidate = exports.randomPosition(radius);
+        candidate.radius = radius;
 
-    it('should return a positive number', function () {
-      var d = util.getDistance(p1, p2);
-      expect(d).to.be.above(-1);
-    });
-  });
-});
+        for (var pi = 0; pi < points.length; pi++) {
+            var distance = exports.getDistance(candidate, points[pi]);
+            if (distance < minDistance) {
+                minDistance = distance;
+            }
+        }
+
+        if (minDistance > maxDistance) {
+            bestCandidate = candidate;
+            maxDistance = minDistance;
+        } else {
+            return exports.randomPosition(radius);
+        }
+    }
+
+    return bestCandidate;
+};
+
+exports.findIndex = function(arr, id) {
+    var len = arr.length;
+
+    while (len--) {
+        if (arr[len].id === id) {
+            return len;
+        }
+    }
+
+    return -1;
+};
+
+exports.randomColor = function() {
+    var color = '#' + ('00000' + (Math.random() * (1 << 24) | 0).toString(16)).slice(-6);
+    var c = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+    var r = (parseInt(c[1], 16) - 32) > 0 ? (parseInt(c[1], 16) - 32) : 0;
+    var g = (parseInt(c[2], 16) - 32) > 0 ? (parseInt(c[2], 16) - 32) : 0;
+    var b = (parseInt(c[3], 16) - 32) > 0 ? (parseInt(c[3], 16) - 32) : 0;
+
+    return {
+        fill: color,
+        border: '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+    };
+};
